e73871a9e6a9ab44f5bef7fd5b959a8169571937
Eylen Kim <47227173+eylenkim@users.noreply.github.com>
2019-06-10 13:30:44 -0700

    contact.php update

--- contact.php
+++ contact.php
@@ -9,9 +9,11 @@ if(isset($_POST['submit'])){
     $subject = "Art By Eylen: Email Inquiry"
     $message = $_POST['message'];
 
+    $emailBody = $name . " sent the following message: " <br> . $message;
+
     $headers = "Art By Eylen - From:" . $from;
 
-    mail($to,$subject,$message,$headers);
+    mail($to,$subject,$emailBody,$headers);
     echo "* Mail Sent. Thank you " . $name . ", I will get in touch with you shortly.";
     
     }
