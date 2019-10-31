b6b2e267b6b19892cf3704b67d4da277ec8993ba
Eylen Kim <47227173+eylenkim@users.noreply.github.com>
2019-10-31 11:57:34 -0700

    modal

--- js/mobileAboutDialog.js
+++ js/mobileAboutDialog.js
@@ -1,20 +1,31 @@
-          var modal = document.querySelector(".modal");
-          var trigger = document.querySelector(".about-trigger");
-          var closeButton = document.querySelector(".close-button"); 
+          const modals = Array.from(document.querySelectorAll('.modal'));
+          const triggers = Array.from(document.querySelectorAll('.trigger'));
+          var closeButtons = Array.from(document.querySelectorAll(".close-button")); 
 
-          function toggleModal() {
-              modal.classList.toggle("show-modal");
+        for (const trigger of triggers) {
+             trigger.addEventListener('click', toggleModal);
           }
 
+        for (const closebutton of closeButtons) {
+             closebutton.addEventListener("click", toggleModal);
+          }
+
+          function toggleModal(event)
+            event.target.querySelector('.modal').classList.toggle("show-modal");
+          }
+
+  
           function windowOnClick(event) {
-              if (event.target === modal) {
-                  toggleModal();
-              }
-              else if (event.target === closeButton) {
-                toggleModal();
-              }
-          }
 
-          trigger.addEventListener("click", toggleModal);
-          closeButton.addEventListener("click", toggleModal);
-          window.addEventListener("click", windowOnClick);
+          if (modals.some((modal) => modal.contains(event.target))) {
+            toggleModal();
+            }
+          }
+         
+         window.addEventListener("click", windowOnClick);
+    
+ 
+
+
+
+ 
