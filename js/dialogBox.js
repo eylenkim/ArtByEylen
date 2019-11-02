d1e4ab3341e4d8b8d639ccad461b7f298a41ee10
Eylen <eylenkim@gmail.com>
2019-11-02 14:32:26 -0700

    Modal Window: Resolved // Onclick: Unresolved

--- js/dialogBox.js
+++ js/dialogBox.js
@@ -2,15 +2,14 @@ const modals = Array.from(document.querySelectorAll('.modal'));
 const triggers = Array.from(document.querySelectorAll('.trigger'));
 var closeButton = document.querySelector(".close-button"); 
 
-//if a trigger is clicked then...
+
 for (const trigger of triggers) {
   trigger.addEventListener('click', toggleModal);
 }
 
-// .. then toggle it's modal
 function toggleModal(event) { event.target.closest('.trigger').querySelector('.modal').classList.toggle("show-modal"); }
 
-  // check if the clicked element is a modal, or in a modal
+
 function windowOnClick(event) {
   if (modals.some((modal) => modal.contains(event.target))) {
     toggleModal();
